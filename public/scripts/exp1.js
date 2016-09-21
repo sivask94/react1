9e1330df29d36707c84e3467e02f801a318ccf9e
Siva <sbtl-hoServer>
2016-09-21 12:34:41 +0530

    Commentlist and commentform has been displayed

--- public/scripts/exp1.js
+++ public/scripts/exp1.js
@@ -2,7 +2,29 @@ var CommentBox = React.createClass({
   render: function() {
     return (
       <div className="commentBox">
-        Hello, world! I am a CommentBox.
+       <h1>COMMENTS</h1>
+        <CommentList />
+        <CommentForm />
+      </div>
+    );
+  }
+});
+
+var CommentList = React.createClass({
+  render: function() {
+    return (
+      <div className="commentList">
+        Hello, world! I am a CommentList.
+      </div>
+    );
+  }
+});
+
+var CommentForm = React.createClass({
+  render: function() {
+    return (
+      <div className="commentForm">
+        Hello, world! I am a CommentForm.
       </div>
     );
   }
