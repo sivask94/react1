4f7282764b275a9c7de2572ff01adc4253c52c4c
Siva <sbtl-hoServer>
2016-09-21 13:52:02 +0530

    optimistic updates has been done

--- public/scripts/exp1.js
+++ public/scripts/exp1.js
@@ -13,6 +13,10 @@ var CommentBox = React.createClass({
     });
   },
   handleCommentSubmit: function(comment) {
+  	var comments = this.state.data;
+    comment.id = Date.now();
+    var newComments = comments.concat([comment]);
+    this.setState({data: newComments});
      $.ajax({
       url: this.props.url,
       dataType: 'json',
@@ -22,6 +26,7 @@ var CommentBox = React.createClass({
         this.setState({data: data});
       }.bind(this),
       error: function(xhr, status, err) {
+      	this.setState({data: comments});
         console.error(this.props.url, status, err.toString());
       }.bind(this)
     });
@@ -36,7 +41,7 @@ this.loadCommentsFromServer();
   render: function() {
     return (
       <div className="commentBox">
-       <h1>COMMENTS</h1>
+     <h1>CO  MMENTS</h1>
         <CommentList data={this.state.data} />
         <CommentForm onCommentSubmit={this.handleCommentSubmit} />
       </div>
