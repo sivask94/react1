fc7e5885ee285ae07a98975a3de26e62f6deddee
Siva <sbtl-hoServer>
2016-09-21 13:13:05 +0530

    fetching from the serverand updating state

--- public/scripts/exp1.js
+++ public/scripts/exp1.js
@@ -1,9 +1,25 @@
 var CommentBox = React.createClass({
+	getInitialState: function() {
+    return {data: []};
+  },
+  componentDidMount: function() {
+    $.ajax({
+      url: this.props.url,
+      dataType: 'json',
+      cache: false,
+      success: function(data) {
+        this.setState({data: data});
+      }.bind(this),
+      error: function(xhr, status, err) {
+        console.error(this.props.url, status, err.toString());
+      }.bind(this)
+    });
+  },
   render: function() {
     return (
       <div className="commentBox">
        <h1>COMMENTS</h1>
-        <CommentList data={this.props.data} />
+        <CommentList data={this.state.data} />
         <CommentForm />
       </div>
     );
@@ -60,6 +76,6 @@ var data = [
   {id: 2, author: "Jordan Walke", text: "This is *another* comment"}
 ];
 ReactDOM.render(
-  <CommentBox data={data} />,
+  <CommentBox url="/api/comments" />,
   document.getElementById('content')
 );
